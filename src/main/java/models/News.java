7cb6cc843a49bee8d6bb41d9f749d50b16273741
AugustineKadima <kadimasam@gmail.com>
2021-08-21 17:45:32 +0300

    generate getters and setters for variables in news

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -16,4 +16,44 @@ public class News {
         news.add(this);
         this.id = news.size();
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHeader() {
+        return header;
+    }
+
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getWritten_by() {
+        return written_by;
+    }
+
+    public void setWritten_by(String written_by) {
+        this.written_by = written_by;
+    }
+
+    public ArrayList<News> getNews() {
+        return news;
+    }
+
+    public void setNews(ArrayList<News> news) {
+        this.news = news;
+    }
 }
