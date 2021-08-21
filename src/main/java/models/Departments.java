ecf306d5bbc78a2c33ef1a7ab58d957690a2f3d1
AugustineKadima <kadimasam@gmail.com>
2021-08-21 17:43:58 +0300

    Create a constructor for news

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,7 @@
 package models;
 
 public class Departments {
+    private String name;
+    private int id;
+    private String manager;
 }
--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,19 @@
 package models;
 
+import java.util.ArrayList;
+
 public class News {
+    private int id;
+    private String header;
+    private String content;
+    private String written_by;
+    ArrayList<News> news = new ArrayList<>();
+
+    public News(String header, String content, String written_by) {
+        this.header = header;
+        this.content = content;
+        this.written_by = written_by;
+        news.add(this);
+        this.id = news.size();
+    }
 }
