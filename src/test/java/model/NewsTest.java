0fdd0f2423247280033de9823653428e848b932b
AugustineKadima <kadimasam@gmail.com>
2021-08-21 18:12:29 +0300

    Add test to confirm asignment of variables

--- src/test/java/model/NewsTest.java
+++ src/test/java/model/NewsTest.java
@@ -16,5 +16,18 @@ public class NewsTest {
         Assertions.assertEquals(true, news instanceof News);
     }
 
-
+    @Test
+    public void allVariablesInNewsClassAreAssignable(){
+        News news = new News(
+                "Angry man Vandalizes Property",
+                "An angry man vandalized property in Nairobi",
+                "Larry Madowo"
+        );
+        news.setHeader("Curfew");
+        news.setContent("Curfew extended to June 2022");
+        news.setWritten_by("Jeff Koinange");
+        Assertions.assertEquals("Curfew", news.getHeader());
+        Assertions.assertEquals("Curfew extended to June 2022", news.getContent());
+        Assertions.assertEquals("Jeff Koinange", news.getWritten_by());
+    }
 }
