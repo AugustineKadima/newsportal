345bbd90d9e105b6dc59b87ac858f25407f416fc
AugustineKadima <kadimasam@gmail.com>
2021-08-22 19:16:16 +0300

    Generate getter and setter for user variables

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -14,4 +14,44 @@ public class Users {
         this.roles = roles;
         this.associated_department = associated_department;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getAssociated_department() {
+        return associated_department;
+    }
+
+    public void setAssociated_department(String associated_department) {
+        this.associated_department = associated_department;
+    }
 }
