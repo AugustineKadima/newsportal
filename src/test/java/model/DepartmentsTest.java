3d20ccbcb74821516170973e2a4472ac21efe1ec
AugustineKadima <kadimasam@gmail.com>
2021-08-22 18:50:47 +0300

    Add test to confirm existence of Department class

--- src/test/java/model/DepartmentsTest.java
+++ src/test/java/model/DepartmentsTest.java
@@ -1,4 +1,19 @@
 package model;
 
+import models.Departments;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class DepartmentsTest {
+
+    @Test
+    public void DepartmentClassExists(){
+        Departments departments = new Departments(
+                "ICT",
+                "Tech team",
+                12);
+        Assertions.assertEquals(true, departments instanceof Departments);
+    }
+
+
 }
