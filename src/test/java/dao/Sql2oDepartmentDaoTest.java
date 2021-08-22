eefbc2d937f96c2613488c305396a81ea6a3bc73
AugustineKadima <kadimasam@gmail.com>
2021-08-23 02:57:42 +0300

    Add setup and teardown methods

--- src/test/java/dao/Sql2oDepartmentDaoTest.java
+++ src/test/java/dao/Sql2oDepartmentDaoTest.java
@@ -1,4 +1,26 @@
 package dao;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 public class Sql2oDepartmentDaoTest {
+
+    private Connection conn;
+    private Sql2oUsersDao departmentDao;
+
+
+    @BeforeAll
+    public void setUp() throws Exception {
+        String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
+        Sql2o sql2o = new Sql2o(connectionString, "sirkadima", "kadima123");
+        departmentDao = new Sql2oUsersDao(sql2o);
+        conn = sql2o.open();
+    }
+
+    @AfterAll
+    public void tearDown() throws Exception {
+        conn.close();
+    }
 }
