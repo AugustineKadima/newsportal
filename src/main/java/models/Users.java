b3912ab0609d9340c6a895fda88223fd27faa25e
AugustineKadima <kadimasam@gmail.com>
2021-08-22 19:15:22 +0300

    Add constructor for users

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,4 +1,17 @@
 package models;
 
 public class Users {
+    private int id;
+    private String name;
+    private String position;
+    private String roles;
+    private String associated_department;
+
+    public Users(int id, String name, String position, String roles, String associated_department) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.roles = roles;
+        this.associated_department = associated_department;
+    }
 }
