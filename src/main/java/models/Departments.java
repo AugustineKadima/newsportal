6ebad3d5725d809d33d8e6ca27aeda84fbc16334
AugustineKadima <kadimasam@gmail.com>
2021-08-22 18:39:53 +0300

    Add departments constructor

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,7 +1,19 @@
 package models;
 
-public class Departments {
+import java.util.ArrayList;
+
+public class Departments{
     private String name;
     private int id;
-    private String manager;
+    private String description;
+    private int number_of_employees;
+    public static ArrayList<Departments> allDepartments = new ArrayList<>();
+
+    public Departments(String name, String description, int number_of_employees) {
+        this.name = name;
+        this.description = description;
+        this.number_of_employees = number_of_employees;
+        allDepartments.add(this);
+        this.id = allDepartments.size();
+    }
 }
