4cb0b41b21d20fc503c7a8753c0fd628269de629
AugustineKadima <kadimasam@gmail.com>
2021-08-25 15:03:23 +0300

    Add app name

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,7 +12,17 @@ import org.sql2o.Sql2o;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+
         Connection conn;
         Sql2oNewsDao newsDao;
         Sql2oUsersDao userDao;
